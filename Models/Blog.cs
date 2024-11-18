05f1c5a0277cebd4d1232f96b7ae10dcb3e7ea95
ngontran123 <huynhkiengquan@gmail.com>
2024-11-19 00:25:34 +0700

    up

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web;
+using HtmlAgilityPack;
+
 
 namespace Ecommerce_Product.Models;
 
@@ -22,4 +25,28 @@ public partial class Blog
     public int CategoryId { get; set; } 
 
     public virtual Category Category { get; set; } = null!;
+
+     public string GetFirstPContent()
+    {
+        if (string.IsNullOrEmpty(Content))
+        {  Console.WriteLine("did empty here");
+            return string.Empty;
+        }
+Console.WriteLine("No empty");
+        var doc = new HtmlDocument();
+        string clean_content=HttpUtility.HtmlDecode(Content);
+        doc.LoadHtml(clean_content);
+
+        var firstP = doc.DocumentNode.SelectNodes("//p")?[1];
+     
+        string fullText="";
+        fullText= firstP != null ? firstP.InnerText : string.Empty;
+        fullText=fullText.Replace("&nbsp;"," ");
+        var words= fullText.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+        if(words.Length<=30)
+        {
+            return fullText;
+        }
+        return string.Join(" ",words.Take(30))+"...";
+    }
 }
