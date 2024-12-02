7be7946598ba5db00f581bbe2c3072e64981463b
quan <huynhkiengquan@gmail.com>
2024-12-02 17:35:45 +0700

    up

---
+++ Support_Service/RazorViewRenderer.cs
@@ -0,0 +1,22 @@
+using System;
+using RazorLight;
+
+namespace Ecommerce_Product.Support_Serive;
+
+public class RazorViewRenderer
+{
+    private readonly IRazorLightEngine _engine;
+
+    public RazorViewRenderer()
+    {
+        _engine = new RazorLightEngineBuilder()
+            .UseEmbeddedResourcesProject(typeof(Program))
+            .UseMemoryCachingProvider()
+            .Build();
+    }
+
+    public async Task<string> RenderViewToStringAsync<T>(string viewName, T model)
+    {
+        return await _engine.CompileRenderAsync(viewName, model);
+    }
+}
